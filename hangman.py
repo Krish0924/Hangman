eabe3198ac377c6de14d4bafe00d189096a1cf4a
Krish <krish.kukreja0924@gmail.com>
2021-06-09 11:05:32 -0400

    made a way to have multiple letter that are the same

--- hangman.py
+++ hangman.py
@@ -1,18 +1,22 @@
 word = "hello"
 bodyPartsLost = 0
 
-def display(letter):
+def display(letters):
     amount = ["_" for _ in range(5)]
     
+    for l in letters:
+        if l in word:
+            for i in range(len(word)):
+                if l == word[i]:
+                    amount[i] = l
+
     print(amount)
-    if letter in word:
-        index = word.index(letter)
-        amount[index] = letter
-        print(amount)
-        
+
+letters = []
 
 while bodyPartsLost <= 7:
     print("try to figure out this word!")
     letter = input("type a letter: ")
-    display(letter)
+    letters.append(letter)
+    display(letters)
     print("you have lost zero body parts out of seven")
