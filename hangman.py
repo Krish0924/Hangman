32b83fb9388e2708e4f2770024be558085b8bb1e
Krish <krish.kukreja0924@gmail.com>
2021-06-09 11:53:49 -0400

    made a losing and winning system

--- hangman.py
+++ hangman.py
@@ -1,16 +1,28 @@
 word = "hello"
 bodyPartsLost = 0
-
+ 
 def display(letters):
-    amount = ["_" for _ in range(5)]
-    
+    underscores = ["_" for _ in range(5)]
+    global bodyPartsLost
+    bodyPartsLost = 0
+
     for l in letters:
         if l in word:
             for i in range(len(word)):
                 if l == word[i]:
-                    amount[i] = l
+                    underscores[i] = l
+        else:
+            bodyPartsLost += 1
 
-    print(amount)
+
+    if "_" not in underscores:
+        print("congrats you found the word it was " + word + "!")
+        return True
+    else:
+        print(underscores)
+        return False
+
+    
 
 letters = []
 
@@ -18,5 +30,7 @@ while bodyPartsLost <= 7:
     print("try to figure out this word!")
     letter = input("type a letter: ")
     letters.append(letter)
-    display(letters)
-    print("you have lost zero body parts out of seven")
+    hasWon = display(letters)
+    if hasWon:
+        break
+    print("you have lost " + str(bodyPartsLost) + " body parts out of 7")
